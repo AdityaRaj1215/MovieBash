b52ed5f98d8a5b583cfde2066466be11d0f5ad2c
Aditya Raj <adityarajchaudhary12@gmail.com>
2024-07-06 19:53:35 +0530

    MovieBash Deployment

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=3da6e4f0d5f131b86a0c196daab40e2e&query=${queryTerm}`
+
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
